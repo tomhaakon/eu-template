ba5cf6931b941b089e12b81b4f213b0f3a09c201
Tom-Håkon Fjeldheim <tom.fjeldheim@gmail.com>
2023-08-27 23:47:28 +0200

    languages

--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ import "./assets/tailwind.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { createI18n } from "vue-i18n";
+
+import en from "./locales/en.json";
+import bg from "./locales/bg.json";
+import no from "./locales/no.json";
+
+const i18n = createI18n({
+	locale: "en",
+	fallbackLocale: "en",
+	messages: { en, bg, no },
+});
 
 import App from "./App.vue";
 import router from "./router";
@@ -9,6 +20,7 @@ import router from "./router";
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(i18n);
 app.use(router);
 
 app.mount("#app");
