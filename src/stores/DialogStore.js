5a066cf641a7f5c30e904ea741c74a95fb2e0625
Tom-Håkon Fjeldheim <tom.fjeldheim@gmail.com>
2023-08-28 23:13:07 +0200

    dialogStore

--- src/stores/DialogStore.js
+++ src/stores/DialogStore.js
@@ -1,7 +1,22 @@
 import { defineStore } from 'pinia';
 
 export const useDialogStoreStore = defineStore('DialogStore', {
-  state: () => ({}),
+  state: () => ({
+    openDialog: null,
+  }),
+  actions: {
+    openContactDialog() {
+      this.openDialog = 'contactDialog';
+    },
+    openLoginDialog() {
+      this.openDialog = 'loginDialog';
+    },
+    openNewsDialog() {
+      this.openDialog = 'newsDialog';
+    },
+    closeDialog() {
+      this.openDialog = null;
+    },
+  },
   getters: {},
-  actions: {},
 });
