2c49f9374d8dbb63e3756baf33f28d876f664abc
Tom-Håkon Fjeldheim <tom.fjeldheim@gmail.com>
2023-09-05 00:50:18 +0200

    update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,21 @@
 <template>
   <main class="mx-auto bg-slate-200 px-4">
-    <TitleComponent :title="$t('pages.about.content-title')" class="pt-4" />
-    <CommonTextComponent :text="$t('pages.about.content')" class="py-10 px-4" />
+    <!-- about -->
+    <TitleComponent :title="$t('pages.about.content-title')" class="pt-4 px-2" />
+    <CommonTextComponent :text="$t('pages.about.content')" class="pt-8 px-4" />
+    <Divider class="bg-color3 my-4" />
+    <!-- goal -->
+    <TitleComponent :title="'Goal of the project'" class="pt-8 px-2" />
+    <CommonTextComponent :text="$t('pages.about.goal')" class="pt-8 px-4" />
+    <Divider class="bg-color3 my-4" />
+    <!-- who the project is for -->
+    <TitleComponent :title="'Who the project is for?'" class="pt-8 px-2" />
+    <CommonTextComponent :text="$t('pages.about.project-is-for')" class="pt-8 px-4" />
+    <Divider class="bg-color3 my-4" />
+    <!-- keywords -->
+    <CommonTextComponent :text="'keywords'" class="font-bold pt-4 px-4" />
+    <CommonTextComponent :text="$t('pages.about.related-words')" class="pb-5 px-4" />
+    <Divider class="bg-color3 my-4" />
   </main>
 </template>
 
@@ -10,4 +24,5 @@
   import TitleComponent from '@/components/TitleComponent.vue';
   import SubTitleComponent from '@/components/SubTitleComponent.vue';
   import CommonTextComponent from '@/components/CommonTextComponent.vue';
+  import Divider from '../components/Divider.vue';
 </script>
